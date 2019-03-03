ad4165e70834abab963535bb6a3bf9379c778415
kohzy <kohzhengyang@gmail.com>
2019-03-03 12:01:59 -0500

    Replaced DebugGUI.cs

--- Assets/CaptainsMess/CaptainsMessDebugGUI.cs
+++ Assets/CaptainsMess/CaptainsMessDebugGUI.cs
@@ -1,123 +1,142 @@
 using System;
 using UnityEngine;
 using UnityEngine.Networking;
-
+using System.Net;
+using System.Net.Sockets;
 public class CaptainsMessDebugGUI : MonoBehaviour
 {
-	private CaptainsMess mess;
-	private CaptainsMessNetworkManager networkManager;
+    private CaptainsMess mess;
+    private CaptainsMessNetworkManager networkManager;
 
-	public void Awake()
-	{
-		mess = FindObjectOfType(typeof(CaptainsMess)) as CaptainsMess;
-		networkManager = GetComponent<CaptainsMessNetworkManager>();
-	}
+    public void Awake()
+    {
+        mess = FindObjectOfType(typeof(CaptainsMess)) as CaptainsMess;
+        networkManager = GetComponent<CaptainsMessNetworkManager>();
+    }
 
-	string NetworkDebugString()
-	{
-		string serverString = "[SERVER]\n";
-		if (NetworkServer.active && networkManager.numPlayers > 0)
-		{
-			serverString += "Hosting at " + Network.player.ipAddress + "\n";
-			serverString += String.Format("Players Ready = {0}/{1}", networkManager.NumReadyPlayers(), networkManager.NumPlayers()) + "\n";
-		}
-		if (networkManager.discoveryServer.running && networkManager.discoveryServer.isServer)
-		{
-			serverString += "Broadcasting: " + networkManager.discoveryServer.broadcastData + "\n";
-		}
+    public string LocalIPAddress()
+    {
+        IPHostEntry host;
+        string localIP = "";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+    }
 
-		string clientString = "[CLIENT]\n";
-		if (networkManager.IsClientConnected())
-		{
-			clientString += "Connected to server: " + networkManager.client.connection.address + "\n";
-		}
-		if (networkManager.discoveryClient.running && networkManager.discoveryClient.isClient)
-		{
-			// Discovered servers
-			clientString += "Discovered servers =";
-			foreach (DiscoveredServer server in networkManager.discoveryClient.discoveredServers.Values)
-			{
-				bool isMe = (server.peerId == networkManager.peerId);
-				clientString += "\n- ";
-				if (isMe) {
-					clientString += "(me) ";
-				}
-				clientString += server.ipAddress + " : " + server.rawData;
-			}
-			clientString += "\n";
+    string NetworkDebugString()
+    {
+        string serverString = "[SERVER]\n";
+        if (NetworkServer.active && networkManager.numPlayers > 0)
+        {
+            serverString += "Hosting at " + LocalIPAddress() + "\n";
+            serverString += String.Format("Players Ready = {0}/{1}", networkManager.NumReadyPlayers(), networkManager.NumPlayers()) + "\n";
+        }
+        if (networkManager.discoveryServer.running && networkManager.discoveryServer.isServer)
+        {
+            serverString += "Broadcasting: " + networkManager.discoveryServer.broadcastData + "\n";
+        }
 
-			// Received broadcasts
-			clientString += "Received broadcasts =";
-			foreach (var entry in networkManager.discoveryClient.receivedBroadcastLog) {
-				clientString += "\n" + entry;
-			}
-			clientString += "\n";
-		}
+        string clientString = "[CLIENT]\n";
+        if (networkManager.IsClientConnected())
+        {
+            clientString += "Connected to server: " + networkManager.client.connection.address + "\n";
+        }
+        if (networkManager.discoveryClient.running && networkManager.discoveryClient.isClient)
+        {
+            // Discovered servers
+            clientString += "Discovered servers =";
+            foreach (DiscoveredServer server in networkManager.discoveryClient.discoveredServers.Values)
+            {
+                bool isMe = (server.peerId == networkManager.peerId);
+                clientString += "\n- ";
+                if (isMe)
+                {
+                    clientString += "(me) ";
+                }
+                clientString += server.ipAddress + " : " + server.rawData;
+            }
+            clientString += "\n";
 
-		return serverString + "\n" + clientString;
-	}
+            // Received broadcasts
+            clientString += "Received broadcasts =";
+            foreach (var entry in networkManager.discoveryClient.receivedBroadcastLog)
+            {
+                clientString += "\n" + entry;
+            }
+            clientString += "\n";
+        }
 
-	void OnGUI()
-	{
-		GUILayout.BeginVertical();
-		{
-			if (networkManager.IsConnected())
-			{
-				GUI.color = Color.red;
-				if (GUILayout.Button("Disconnect", GUILayout.Width(200), GUILayout.Height(100)))
-				{
-					mess.Cancel();
-				}
-				GUI.color = Color.white;
-			}
-			else if (networkManager.IsBroadcasting() || networkManager.IsJoining())
-			{
-				GUI.color = Color.yellow;
-				if (GUILayout.Button("Cancel", GUILayout.Width(200), GUILayout.Height(100)))
-				{
-					mess.Cancel();
-				}
-				GUI.color = Color.white;
-			}
-			else
-			{
-				GUILayout.BeginHorizontal();
-				{
-					GUI.color = Color.green;
-					if (GUILayout.Button("Auto Connect", GUILayout.Width(200), GUILayout.Height(120)))
-					{
-						mess.AutoConnect();
-					}
-					GUI.color = Color.white;
+        return serverString + "\n" + clientString;
+    }
 
-					GUILayout.BeginVertical();
-					GUILayout.FlexibleSpace();
-					GUILayout.Label("... OR ...");
-					GUILayout.FlexibleSpace();
-					GUILayout.EndVertical();
+    void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        {
+            if (networkManager.IsConnected())
+            {
+                GUI.color = Color.red;
+                if (GUILayout.Button("Disconnect", GUILayout.Width(200), GUILayout.Height(100)))
+                {
+                    mess.Cancel();
+                }
+                GUI.color = Color.white;
+            }
+            else if (networkManager.IsBroadcasting() || networkManager.IsJoining())
+            {
+                GUI.color = Color.yellow;
+                if (GUILayout.Button("Cancel", GUILayout.Width(200), GUILayout.Height(100)))
+                {
+                    mess.Cancel();
+                }
+                GUI.color = Color.white;
+            }
+            else
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUI.color = Color.green;
+                    if (GUILayout.Button("Auto Connect", GUILayout.Width(200), GUILayout.Height(120)))
+                    {
+                        mess.AutoConnect();
+                    }
+                    GUI.color = Color.white;
 
-					GUILayout.BeginVertical();
-					{
-						if (GUILayout.Button("Host", GUILayout.Width(150), GUILayout.Height(60)))
-						{
-							mess.StartHosting();
-						}
-						if (GUILayout.Button("Join", GUILayout.Width(150), GUILayout.Height(60)))
-						{
-							mess.StartJoining();
-						}
-					}
-					GUILayout.EndVertical();
-				}
-				GUILayout.EndHorizontal();
-			}
-		}
-		GUILayout.EndVertical();
+                    GUILayout.BeginVertical();
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label("... OR ...");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndVertical();
 
-		// Debug log
-		var style = new GUIStyle();
-		style.fontSize = 20;
-		style.normal.textColor = Color.yellow;
-		GUILayout.Label(NetworkDebugString(), style);
-	}
+                    GUILayout.BeginVertical();
+                    {
+                        if (GUILayout.Button("Host", GUILayout.Width(150), GUILayout.Height(60)))
+                        {
+                            mess.StartHosting();
+                        }
+                        if (GUILayout.Button("Join", GUILayout.Width(150), GUILayout.Height(60)))
+                        {
+                            mess.StartJoining();
+                        }
+                    }
+                    GUILayout.EndVertical();
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+        GUILayout.EndVertical();
+
+        // Debug log
+        var style = new GUIStyle();
+        style.fontSize = 20;
+        style.normal.textColor = Color.yellow;
+        GUILayout.Label(NetworkDebugString(), style);
+    }
 }
